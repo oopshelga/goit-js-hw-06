990b17f9f8d7d6e5cfabfc4eea818fe1a15b6228
oopshelga <134417737+oopshelga@users.noreply.github.com>
2023-09-27 21:12:37 +0300

    changed sixth task

--- js/task-06.js
+++ js/task-06.js
@@ -9,15 +9,26 @@
 
 // Яка кількість символів повинна бути в інпуті, зазначається в його атрибуті data-length.
 // Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна кількість - червоним.
+// const validationInput = document.querySelector("#validation-input");
+// const expectedLength = parseInt(validationInput.getAttribute("data-length"));
 
+// validationInput.addEventListener("blur", () => {
+// const inputValue = validationInput.value.trim();
 
-
+//   if (inputValue.length === expectedLength) {
+//     validationInput.classList.remove("invalid");
+//     validationInput.classList.add("valid");
+//   } else {
+//     validationInput.classList.remove("valid");
+//     validationInput.classList.add("invalid");
+//   }
+// });
 
 const validationInput = document.querySelector('#validation-input');
 
 validationInput.addEventListener('blur', () => {
     const dataLength = Number(validationInput.dataset.length);
-    const inputValue = validationInput.value.length;
+    const inputValue = validationInput.trim();
     
 if (inputValue.length === dataLength) {
     validationInput.classList.remove('invalid');
